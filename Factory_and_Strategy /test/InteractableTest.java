9173937866573c2716a45d0b37cce612483c80a0
joec1368 <chenforwork1368@gmail.com>
2022-07-21 13:19:55 +0800

    change the way of creation the world

--- Factory_and_Strategy /test/InteractableTest.java
+++ Factory_and_Strategy /test/InteractableTest.java
@@ -1,4 +1,7 @@
 import dp.*;
+import nonused.Accessory;
+import nonused.PoliteWorld;
+import nonused.RudeWorld;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -29,5 +32,39 @@ public class InteractableTest {
         assertEquals(world.getTree().getHeight(), 11);
         world.getTree().waterIt();
         assertEquals(world.getTree().getHeight(), 12);
+    }
+
+    @ParameterizedTest
+    @EnumSource(World.Type.class)
+    public void TestTreeGrow_Accessory() {
+        Accessory world = new PoliteWorld();
+        assertEquals(world.tree_GetHeight(), 10);
+        world.tree_WaterIt();
+        assertEquals(world.tree_GetHeight(), 11);
+        world.tree_WaterIt();
+        assertEquals(world.tree_GetHeight(), 12);
+        world = new RudeWorld();
+        assertEquals(world.tree_GetHeight(), 10);
+        world.tree_WaterIt();
+        assertEquals(world.tree_GetHeight(), 11);
+        world.tree_WaterIt();
+        assertEquals(world.tree_GetHeight(), 12);
+    }
+
+    @ParameterizedTest
+    @EnumSource(World.Type.class)
+    public void TestStoneHarden_Accessory() {
+        Accessory world = new PoliteWorld();
+        assertEquals(world.stone_GetSolidity(), 5);
+        world.stone_Harden();
+        assertEquals(world.stone_GetSolidity(), 6);
+        world.stone_Harden();
+        assertEquals(world.stone_GetSolidity(), 7);
+        world = new RudeWorld();
+        assertEquals(world.stone_GetSolidity(), 5);
+        world.stone_Harden();
+        assertEquals(world.stone_GetSolidity(), 6);
+        world.stone_Harden();
+        assertEquals(world.stone_GetSolidity(), 7);
     }
 }
